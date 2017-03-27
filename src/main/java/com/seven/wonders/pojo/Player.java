a77b0c1e7845f890e760d6ba53c822eb3eca14f7
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-03-28 01:32:24 +0300

    fix redirect, player_name & game_name

--- src/main/java/com/seven/wonders/pojo/Player.java
+++ src/main/java/com/seven/wonders/pojo/Player.java
@@ -4,17 +4,17 @@ package com.seven.wonders.pojo;
  * Created by user on 25.02.2017.
  */
 public class Player {
-    private String name;
 
-    public boolean isAdmin() {
-        return isAdmin;
+    private String name;
+    private Boolean isAdmin;
+
+    public Player() {
     }
 
-    public void setAdmin(boolean isAdmin) {
+    public Player(String name, Boolean isAdmin) {
+        this.name = name;
         this.isAdmin = isAdmin;
     }
-
-    private boolean isAdmin;
 
     public String getName() {
         return name;
@@ -22,5 +22,13 @@ public class Player {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
     }
 }
