24ba9ebdec6b645c85d163caeb876396701796ca
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-04-06 15:08:00 +0300

    add leaveGame() method in NewgameController.java for returning to login form

--- src/main/java/com/seven/wonders/controller/NewgameController.java
+++ src/main/java/com/seven/wonders/controller/NewgameController.java
@@ -2,12 +2,16 @@ package com.seven.wonders.controller;
 
 import com.seven.wonders.core.Application;
 import com.seven.wonders.core.Session;
+import com.seven.wonders.pojo.entity.Game;
+import com.seven.wonders.pojo.entity.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by Pavel Ruban on 15.03.2017.
@@ -31,9 +35,21 @@ public class NewgameController {
     }
 
     @RequestMapping(value = "/newgame/leave", method = {RequestMethod.GET,RequestMethod.POST})
-    public String login(Model model){
-        model.addAttribute("allGames", application.getAllGames().values());
-        return "redirect:login";
+    public String leaveGame(){
+        String currentGameId = session.getCurrentGameId();
+        Game currentGame = application.getAllGames().get(currentGameId);
+        Player currentPlayer = session.getCurrentPlayer();
+        List<Player> players = currentGame.getPlayers();
+
+        if (players.contains(currentPlayer)) {
+            players.remove(currentPlayer);
+        }
+
+        if (players.size() == 0) {
+            application.getAllGames().remove(currentGameId);
+        }
+
+        return "redirect:/login";
     }
 
 }
