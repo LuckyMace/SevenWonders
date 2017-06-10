189d3f5670530f0fd2a4ee9a7d2d6a60848b8634
LuckyMace <mace2windu@gmail.com>
2017-06-11 00:03:01 +0300

    Add new values to enum Card.

--- src/main/java/com/seven/wonders/pojo/enumer/Card.java
+++ src/main/java/com/seven/wonders/pojo/enumer/Card.java
@@ -254,8 +254,8 @@ public enum Card {
             , 4, 3, CardType.COMMERCIAL, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_18.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_18.jpg", null, null),
     CARD_3_19 (319, "3_19", "Торговая палата", Arrays.asList(new Resource(ResourceType.CLAY, 2), new Resource(ResourceType.GLASS, 0), new Resource(ResourceType.GOLD, 0), new Resource(ResourceType.ORE, 0), new Resource(ResourceType.PAPIRUS, 1), new Resource(ResourceType.STONE, 0), new Resource(ResourceType.TEXTILE, 0), new Resource(ResourceType.WOOD, 0))
             , 6, 3, CardType.COMMERCIAL, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_19.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_19.jpg", null, null),
-    CARD_3_20 (320, "3_20", "Гавань", Arrays.asList(new Resource(ResourceType.CLAY, 2), new Resource(ResourceType.GLASS, 0), new Resource(ResourceType.GOLD, 0), new Resource(ResourceType.ORE, 0), new Resource(ResourceType.PAPIRUS, 1), new Resource(ResourceType.STONE, 0), new Resource(ResourceType.TEXTILE, 0), new Resource(ResourceType.WOOD, 0))
-            , 3, 3, CardType.COMMERCIAL, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_19.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_19.jpg", null, null),
+    CARD_3_20 (320, "3_20", "Гавань", Arrays.asList(new Resource(ResourceType.CLAY, 0), new Resource(ResourceType.GLASS, 0), new Resource(ResourceType.GOLD, 0), new Resource(ResourceType.ORE, 1), new Resource(ResourceType.PAPIRUS, 0), new Resource(ResourceType.STONE, 0), new Resource(ResourceType.TEXTILE, 1), new Resource(ResourceType.WOOD, 1))
+            , 3, 3, CardType.COMMERCIAL, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_20.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_20.jpg", null, Arrays.asList(CARD_2_26, CARD_2_27, CARD_2_28)),
 
 
     ;
