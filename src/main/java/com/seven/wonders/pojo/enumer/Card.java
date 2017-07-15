53faac8ca4d47ee729e7a6fce550b85463411c60
LuckyMace <mace2windu@gmail.com>
2017-07-16 00:51:23 +0300

    Add Card enum values.

--- src/main/java/com/seven/wonders/pojo/enumer/Card.java
+++ src/main/java/com/seven/wonders/pojo/enumer/Card.java
@@ -278,6 +278,8 @@ public enum Card {
             , 3, 3, CardType.MILITARY, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_30.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_30.jpg", null, Arrays.asList(CARD_2_45, CARD_2_46)),
     CARD_3_31 (331, "3_31", "Осадные орудия", Arrays.asList(new Resource(ResourceType.CLAY, 3), new Resource(ResourceType.GLASS, 0), new Resource(ResourceType.GOLD, 0), new Resource(ResourceType.ORE, 0), new Resource(ResourceType.PAPIRUS, 0), new Resource(ResourceType.STONE, 0), new Resource(ResourceType.TEXTILE, 0), new Resource(ResourceType.WOOD, 1))
             , 5, 3, CardType.MILITARY, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_31.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_31.jpg", null, Arrays.asList(CARD_2_45, CARD_2_46)),
+    CARD_3_32 (332, "3_32", "Академия", Arrays.asList(new Resource(ResourceType.CLAY, 0), new Resource(ResourceType.GLASS, 1), new Resource(ResourceType.GOLD, 0), new Resource(ResourceType.ORE, 0), new Resource(ResourceType.PAPIRUS, 0), new Resource(ResourceType.STONE, 3), new Resource(ResourceType.TEXTILE, 0), new Resource(ResourceType.WOOD, 0))
+            , 3, 3, CardType.SCIENTIFIC, "src\\main\\webapp\\WEB-INF\\static\\img\\cards\\3_32.jpg", "src\\main\\webapp\\WEB-INF\\static\\img\\small_cards\\3_32.jpg", null, Arrays.asList(CARD_2_43, CARD_2_44)),
 
 
     ;
